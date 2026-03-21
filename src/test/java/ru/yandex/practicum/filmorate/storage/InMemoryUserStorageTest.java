f8f247d408770aa01654a3cd465c767f46438279
valery <vborodina@emdev.ru>
2026-03-22 02:59:04 +0300

    fix checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorageTest.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
